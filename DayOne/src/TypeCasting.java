76ab38a3ed947e60499e2d419dbdd9f4cb7d1d81
vijay <vijay@VIJAY>
2023-04-17 12:04:48 +0530

    tax

---
+++ DayOne/src/TypeCasting.java
@@ -0,0 +1,21 @@
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		int aa=78363;
+      byte a=(byte)78363;//Narrow type casting
+      System.out.println(aa);
+      System.out.println(a);
+      double db=1.55555448741485;
+      float fl=(float)db;//Narrow type casting
+      System.out.println(db);
+      System.out.println(fl);
+    
+      int b=12;//widening Type Casting
+      System.out.println(b);
+      
+      
+
+	}
+
+}
