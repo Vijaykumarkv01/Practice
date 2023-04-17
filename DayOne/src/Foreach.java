6d2e2d487a6d8b5de1e4009b6f02c6e098bceb27
vijay <vijay@VIJAY>
2023-04-17 12:49:23 +0530

    Foreach

---
+++ DayOne/src/Foreach.java
@@ -0,0 +1,15 @@
+
+public class Foreach {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   int[] numbers = {3, 9, 5, -5,7,8,4,10};
+		    
+		    // for each loop 
+		    for (int number: numbers) {
+		      System.out.println(number);
+		    }
+
+	}
+
+}
