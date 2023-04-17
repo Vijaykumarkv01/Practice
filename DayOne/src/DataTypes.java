2f99102870fbc87f3e27a1889cb6a9544d907ad9
vijay <vijay@VIJAY>
2023-04-17 08:55:19 +0530

    DayOne

---
+++ DayOne/src/DataTypes.java
@@ -0,0 +1,23 @@
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		byte a=1;
+		short b=12;
+		int c=100;
+		long d=1523564L;
+		float e=1.56475f;
+		double f=1542.2546456545;
+		char g='c';
+		boolean h=true;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+	}
+
+}
